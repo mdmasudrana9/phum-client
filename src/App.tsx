6e6989d09893e58d1e2d2b87bb783f9901406285
masud-rana9 <rana61960@gmail.com>
2025-01-04 21:03:27 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "./components/layout/MainLayout";
+
+const App = () => {
+  return (
+    <div className="text-xl">
+      <MainLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>,
+  },
+]);
+
+export default router;
